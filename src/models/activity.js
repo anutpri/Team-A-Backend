fdc0b58a2519985aae8a1595f73d5901d26bcfdc
anutpri <anut.pri@gmail.com>
2023-05-12 14:42:17 +0700

    finish .patch activity

--- src/models/activity.js
+++ src/models/activity.js
@@ -5,7 +5,7 @@ const activitySchema = new mongoose.Schema(
     activity_name: { type: String, required: true },
     // description: String,
     // username: { type: mongoose.Schema.Types.ObjectId, required: true, ref:"user"},
-    // activity_type: { type: String, enum: ['Running', 'Walking', 'Bike cycling', 'Weight training', 'Dancing'], required: true},
+    activity_type: { type: String, enum: ['Running', 'Walking', 'Bike cycling', 'Weight training', 'Dancing'], required: true},
     // distance: { type: Number }, duration_time: { type: Number, required: true, min: 10 },
     // start_date_time: { type: Date, required: true },
     // finish_date_time: { type: Date, required: true },
--- src/routers/activities.js
+++ src/routers/activities.js
@@ -45,8 +45,21 @@ activityRouter.post("/", async (req, res) => {
 
 activityRouter.patch("/:activityId", async (req, res) => {
   try {
-    // set update logic soon
-    res.send("update");
+    const { activityId } = req.params;
+    const { activity_name } = req.body;
+    const { activity_type } = req.body;
+    const updatedActivity = await activityModel.findOneAndUpdate(
+      { _id: activityId },
+      { activity_name },
+      { activity_type },
+      { new: true }
+    );
+
+    if (!updatedActivity) {
+      return res.status(404).send("Activity not found");
+    }
+
+    return res.send(updatedActivity.toJSON());
   } catch (error) {
     console.error("Error updating activity:", error);
     res.status(500).send("Internal Server Error");
